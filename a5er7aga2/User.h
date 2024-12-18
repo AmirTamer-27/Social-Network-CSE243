213faa39dac3b1ab440108a7cda8cfc7d2144649
walmartcurry <amirtamer31@gmail.com>
2024-12-18 15:51:27 +0200

    Update User.h

--- a5er7aga2/User.h
+++ a5er7aga2/User.h
@@ -1,39 +1,30 @@
-#ifndef USER
-#define USER
+#ifndef GRAPH
+#define GRAPH
 #include <iostream>
+#include <list>
+#include <set>
 #include "STLs.h"
+#include "User.h"
 using namespace std;
-class User {
-private:
-    string username;
-    string password;
-    int age;
+
+class Graph {
+
 
 public:
-    LinkedList friends;
-    HashTable pending;
-    HashTable requests;
-    User() {}
-    User(string username, string password, int age) :username(username), password(password), age(age) {}
-    LinkedList& getfriends() {
-        return friends;
-    }
-    string& getusername() {
-        return username;
-    }
-    string& getpassword() {
-        return password;
-    }
-    int getage() {
-        return age;
-    }
-    HashTable& getPending() {
-        return pending;
-    }
-    HashTable& getRequests() {
-        return requests;
-    }
-    ~User() {
-    }
+    Dequeue<User*> users;
+    Graph() {}
+
+    // void signup();
+    // int login();
+    void showFriends(int index);
+    int handleUser(int);
+    void handleRequests(const int  ,const  int  ,const  int );
+    void sendRequest(const int current, const int index);
+    void removeFriend(const int currUser, const int index);
+    void BFS(int, CustomMap& Visited);
+    void showRequests(int);
+    void search(string username, int currentUser);
+    void peopleYouMayKnow(int);
+    void viewUser(int currentUser, int userIndex);
 };
 #endif
