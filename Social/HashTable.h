46f7e1151d061272956ff494162ef0cb06eaf77b
walmartcurry <amirtamer31@gmail.com>
2024-12-22 22:47:30 +0200

    Add files via upload

---
+++ Social/HashTable.h
@@ -0,0 +1,141 @@
+#ifndef HASHTABLE
+#define HASHTABLE
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+#include "Dequeue.h"
+using namespace std;
+const int TableSize = 16383;
+template<typename HashElement>
+
+class HashTable {
+
+private:
+
+    class Node {
+    public:
+        HashElement value; // Student name
+        Node* next;
+        Node(HashElement value) {
+            next = nullptr;
+            this->value = value;
+        }
+        // Node(){next = nullptr;value=-1;}
+    };
+
+    
+    Node** table;
+    int hash(HashElement value) {
+        return value % TableSize;
+    }
+public:
+int numberOfElements;
+  HashTable() : numberOfElements(0) {
+    table = new (nothrow) Node*[TableSize];
+}
+
+
+    void insert(HashElement value) {
+        int loc = hash(value);
+        if (table[loc] == nullptr) {
+            table[loc] = new Node(value);
+            numberOfElements++;
+            return;
+        }
+        if (table[loc]->value == value) {
+            cerr << "Item already exists";
+            return;
+        }
+        else {
+            Node* ptr = table[loc];
+            while (ptr->next != nullptr)
+                ptr = ptr->next;
+            ptr->next = new Node(value);
+        }
+    }
+
+    void remove(HashElement value) {
+        int loc = hash(value);
+        if (table[loc]->value == value) {
+            delete table[loc];
+        }
+        else {
+            Node* pred;
+            Node* ptr = table[loc];
+            while (ptr != nullptr && ptr->value != value) {
+                pred = ptr;
+                ptr = ptr->next;
+            }
+            if (ptr == nullptr) {
+                cout << "Item not found";
+                return;
+            }
+            else {
+                pred->next = ptr->next;
+                delete ptr;
+
+            }
+        }
+    }
+    void getIndices(Dequeue<int>& requestingUsers) {
+         int count = 1;
+         short x;
+         for (int i = 0; i < TableSize; i++) {
+             if (table[i] == nullptr)
+                 continue;
+             count++;
+             requestingUsers.push_back(table[i]->value);
+             if (table[i]->next != nullptr) {
+                 Node* ptr = table[i]->next;
+                 while (ptr != nullptr) {
+                     count++;
+                     requestingUsers.push_back(ptr->value);
+                     ptr = ptr->next;
+                 }
+             }
+         }
+     }
+    bool found(HashElement value) {
+        if (table[hash(value)] == nullptr)
+            return false;
+        Node* ptr = table[hash(value)];
+        while (ptr != nullptr) {
+            if (ptr->value == value)
+                return true;
+            ptr = ptr->next;
+        }
+        return false;
+    }
+        void print(Dequeue<int>& requestingUsers) {
+         int count = 1;
+         short x;
+         for (int i = 0; i < TableSize; i++) {
+             if (table[i] == nullptr)
+                 continue;
+             count++;
+             requestingUsers.push_back(table[i]->value);
+             if (table[i]->next != nullptr) {
+                 Node* ptr = table[i]->next;
+                 while (ptr != nullptr) {
+                     count++;
+                     requestingUsers.push_back(ptr->value);
+                     ptr = ptr->next;
+                 }
+             }
+         }
+     }
+     ~HashTable() {
+    for (int i = 0; i < TableSize; ++i) {
+        Node* current = table[i];
+        while (current != nullptr) {
+            Node* nextNode = current->next;
+            delete current; // Delete the current node
+            current = nextNode; // Move to the next node
+        }
+    }
+    delete[] table; // Delete the table array
+}
+
+};
+#endif
---
+++ Social/LinkedList.h
@@ -0,0 +1,172 @@
+#ifndef LINKEDLIST
+#define LINKEDLIST
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+template<typename ElementType>
+class LinkedList {
+private:
+    // Nested node class
+    class Node {
+    public:
+
+        ElementType data;
+        Node* next; // How does it refer to itself although it isn't created yet?
+
+        Node() : next(0) {}
+        Node(ElementType dataValue) : data(dataValue), next(0) {}
+    };
+
+    typedef Node* NodePointer;
+    NodePointer first;
+    int mySize;
+
+public:
+    LinkedList() : first(0), mySize(0) {}
+
+    LinkedList(const LinkedList<int>& origList) {
+        if (mySize == 0) return;
+        NodePointer origPtr, lastPtr;
+        first = new Node(origList.first->data);  // copy first node
+        lastPtr = first;
+        origPtr = origList.first->next;
+        while (origPtr != 0)
+        {
+
+
+            lastPtr->next = new Node(origPtr->data);
+            origPtr = origPtr->next;
+            lastPtr = lastPtr->next;
+        }
+    }
+    ~LinkedList() {
+        NodePointer prev = first, ptr;
+        while (prev != 0)
+        {
+            ptr = prev->next;
+            delete prev;
+            prev = ptr;
+        }
+    }
+    void reverse() {
+        NodePointer ptr = first, nptr = first->next;
+        while (ptr != 0)
+        {
+            NodePointer sptr = nptr->next;
+            sptr = ptr;
+            ptr = nptr->next;
+            nptr->next = ptr;
+            sptr->next = nptr;
+            ptr = ptr->next;
+            nptr = nptr->next;
+            sptr = sptr->next;
+        }
+    }
+    const LinkedList& operator=(const LinkedList<int>& rightSide) {
+        mySize = rightSide.mySize;
+
+        if (mySize == 0) {
+            first = 0;
+            return *this;
+        }
+        if (this != &rightSide)
+        {
+            this->~LinkedList<int>();
+            NodePointer origPtr, lastPtr;
+            first = new Node(rightSide.first->data);  // copy first node
+            lastPtr = first;
+            origPtr = rightSide.first->next;
+
+            while (origPtr != 0)
+            {
+                lastPtr->next = new Node(origPtr->data);
+                origPtr = origPtr->next;
+                lastPtr = lastPtr->next;
+            }
+        }
+        return *this;
+    }
+
+    bool empty() {
+        return mySize == 0;
+    }
+    void insert(ElementType dataVal, int index) {
+        if (index < 0 || index > mySize)
+        {
+            cerr << "Illegal location to insert -- " << index << endl;
+            return;
+        }
+
+        mySize++;
+        NodePointer newPtr = new Node(dataVal), predPtr = first;
+        if (index == 0)
+        {
+            newPtr->next = first;
+            first = newPtr;
+        }
+        else {
+            for (int i = 1; i < index; i++)
+                predPtr = predPtr->next;
+            newPtr->next = predPtr->next;
+            predPtr->next = newPtr;
+        }
+    }
+    void erase(int index) {
+        if (index < 0 || index >= mySize)
+        {
+            cerr << "Illegal location to delete -- " << index << endl;
+            return;
+        }
+        mySize--;
+        NodePointer ptr,
+            predPtr = first;
+        if (index == 0)
+        {
+            ptr = first;
+            first = ptr->next;
+            delete ptr;
+        }
+        else {
+            for (int i = 1; i < index; i++)
+                predPtr = predPtr->next;
+            ptr = predPtr->next;
+            predPtr->next = ptr->next;
+            delete ptr;
+        }
+    }
+    int size() {
+        return mySize;
+    }
+    int search(ElementType dataVal) {
+        int loc;
+        NodePointer tempP = first;
+        for (loc = 0; loc < mySize; loc++)
+            if (tempP->data == dataVal)
+                return loc;
+            else
+                tempP = tempP->next;
+        return -1;
+    }
+    void display(ostream& out) const {
+        NodePointer ptr = first;
+        while (ptr != 0)
+        {
+            out << ptr->data << "  ";
+            ptr = ptr->next;
+        }
+    }
+    NodePointer begin()
+    {
+        return first;
+    }
+};
+
+ostream& operator<<(ostream& out, const LinkedList<int>& aList)
+{
+    aList.display(out);
+    return out;
+}
+#endif
